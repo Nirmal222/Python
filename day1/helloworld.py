e3b4bf1ba6d9d163eddc6575ada8d2ca8be18298
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-07 21:56:27 +0530

    Installed and learned about indentation

---
+++ day1/helloworld.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+# indentation
+    if 5 > 2:
+    print("Five is greater than two!")
+    # The above code fine with respect to indentation
+    if 5 > 2:
+    print("Five is greater than two!")
+    # The above code throws error as it is not properly indented 
+    # note : The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+    #------------------------------------------------------
+    if 5 > 2:
+        print("Five is greater than two!")
+                print("Five is greater than two!")
+    # You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+    #------------------------------------------------------
