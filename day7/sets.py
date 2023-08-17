19b827f509a4059aaf50e40c3d9cca41af02887c
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-17 23:48:19 +0530

    Sets

--- day7/sets.py
+++ day7/sets.py
@@ -35,3 +35,44 @@ thisset.update(tropical)
 print(thisset)
 
 # The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.). 
+
+
+# Remove Item
+# To remove an item in a set, use the remove(), or the discard() method.
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+# Note: If the item to remove does not exist, remove() will raise an error.
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+# Note: If the item to remove does not exist, discard() will NOT raise an error.
+# The clear() method empties the set:
+
+
+# The union() method returns a new set with all items from both sets:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+# The update() method inserts the items in set2 into set1:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
+# The intersection_update() method will keep only the items that are present in both sets.
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+
+print(x) #apple
