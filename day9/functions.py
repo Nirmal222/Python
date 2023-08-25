ed75a4a5fb735d530d8785f4e71d6186d273ec4d
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-25 23:57:45 +0530

    Functions

---
+++ day9/functions.py
@@ -0,0 +1,14 @@
+# Functions 
+# Arbitrary Arguments, *args
+# If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+# This way the function will receive a tuple of arguments, and can access the items accordingly:
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+# key value arguments where order does not matter
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
