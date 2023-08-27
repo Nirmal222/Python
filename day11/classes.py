4879eb066a4b0f39add7a9aaaff6cd0cc7a238d5
nirmal kumar <nirmalkumargurajada@gmail.com>
2023-08-27 21:34:33 +0530

    Classes

---
+++ day11/classes.py
@@ -0,0 +1,43 @@
+# Python Classes/Objects
+# Python is an object oriented programming language.
+# Almost everything in Python is an object, with its properties and methods.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+# The __init__() Function
+# The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+# To understand the meaning of classes we have to understand the built-in __init__() function.
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+# The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+# It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
+
+# Object Methods
+# Insert a function that prints a greeting, and execute it on the p1 object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
